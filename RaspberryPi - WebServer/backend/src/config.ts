af017f7e905e9aafe395358700f850dcebffea56
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2025-02-26 21:40:05 +0100

    added types in config.ts

--- RaspberryPi - WebServer/backend/src/config.ts
+++ RaspberryPi - WebServer/backend/src/config.ts
@@ -2,18 +2,18 @@ import dotenv from "dotenv";
 import path, { resolve } from "path";
 import { fileURLToPath } from "url";
 import NodeEnv from "./types/NodeEnvType.js";
-const NODE_DEFAULT_PORT = 5000;
-const VITE_DEFAULT_PORT = 5173;
+const NODE_DEFAULT_PORT: number = 5000;
+const VITE_DEFAULT_PORT: number = 5173;
 
-const rootDirName = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "..");
+const rootDirName: string = path.resolve(path.dirname(fileURLToPath(import.meta.url)), "..");
 
 dotenv.config({ path: resolve(rootDirName, "..", ".env") });
-const isNodeEnvProd = process.env.NODE_ENV === "prod";
+const isNodeEnvProd: boolean = process.env.NODE_ENV === "prod";
 const nodeEnv: NodeEnv = isNodeEnvProd ? "prod" : "dev";
 
 dotenv.config({ path: resolve(rootDirName, ".env"), override: true });
-const port = (isNodeEnvProd ? process.env.PROD_PORT : process.env.DEV_PORT) || NODE_DEFAULT_PORT;
-const viteDevPort = process.env.VITE_DEV_PORT || VITE_DEFAULT_PORT;
+const port: number = (isNodeEnvProd ? Number(process.env.PROD_PORT) : Number(process.env.DEV_PORT)) || NODE_DEFAULT_PORT;
+const viteDevPort: number = Number(process.env.VITE_DEV_PORT) || VITE_DEFAULT_PORT;
 
 const config = {
     root_dir: rootDirName,
