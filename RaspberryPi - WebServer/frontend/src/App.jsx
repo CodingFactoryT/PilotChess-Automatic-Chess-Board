050fee494abab4d6cdbb0e2e668f426e37c46150
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2025-02-28 00:05:55 +0100

    added routes to frontend

--- RaspberryPi - WebServer/frontend/src/App.jsx
+++ RaspberryPi - WebServer/frontend/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/homePage';
+import LoginPage from './pages/loginPage';
+import ProfilePage from './pages/profilePage';
+import WifiConfigurationPage from './pages/wifiConfigurationPage';
+import NotFoundPage from './pages/notFoundPage';
+
 function App() {
 	return (
-		<>
-			<p>Hello World!</p>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<HomePage/>} />
+				<Route path="/login" element={<LoginPage/>} />
+				<Route path="/profile" element={<ProfilePage/>} />
+				<Route path="/wifi-configuration" element={<WifiConfigurationPage/>} />
+				<Route path="/*" element={<NotFoundPage/>} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
