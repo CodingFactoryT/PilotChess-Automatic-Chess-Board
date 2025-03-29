248009c8d9a5b785e53ee52e4dc71ade5fe10d35
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2025-03-30 00:28:00 +0100

    test

--- RaspberryPi-WebServer/backend/src/api/controllers/BoardController.js
+++ RaspberryPi-WebServer/backend/src/api/controllers/BoardController.js
@@ -4,6 +4,7 @@ export async function waitForPieceMovement() {
 	const fromPosition = null;
 	//while (!(fromPosition = await hasTileGridChanged()));
 	while (true) {
+		console.log("Connected, starting fetching data");
 		await hasTileGridChanged();
 	}
 	console.log(fromPosition);
@@ -13,8 +14,8 @@ let lastReadPositioning = null;
 async function hasTileGridChanged() {
 	try {
 		const response = await fetchArduino("REQ:READ:");
+		console.log(response);
 		const boardPositioning = response.split(",")[1];
-		console.log(boardPositioning);
 		lastReadPositioning = boardPositioning;
 	} catch (error) {
 		console.log(error);
