5cb6aef28e5047bae44a5020561d3ad48e4f9231
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2025-04-21 18:34:00 +0200

    refactored BoardController

--- RaspberryPi-WebServer/backend/src/api/controllers/BoardController.js
+++ RaspberryPi-WebServer/backend/src/api/controllers/BoardController.js
@@ -1,6 +1,7 @@
 import fetchArduino from "../services/ArduinoCommunicator.js";
 import { hexToBinary64, concatZeroesUntilSizeMatches } from "../helpers/util.js";
 import { BISHOP, Chess, KING, KNIGHT, PAWN, QUEEN, ROOK } from "chess.js";
+import BoardPosition from "../helpers/BoardPosition.js";
 
 export default class BoardController {
 	static #instance = null;
@@ -11,7 +12,7 @@ export default class BoardController {
 			throw new Error("Use BoardController.getInstance() instead of new.");
 		}
 
-		this.lastReadPositioning = null;
+		//this.lastReadPositioning = null;
 		this.board = new Chess(fen);
 		BoardController.#instance = this;
 	}
@@ -32,9 +33,16 @@ export default class BoardController {
 	 * move: e.g. "a2a3"
 	 */
 	async moveOpponentsPiece(move) {
-		const fromPosition = move.substring(0, 2);
-		const toPosition = move.substring(2, 4);
-		const pieceToMove = this.board.get(fromPosition);
+		const fromPosition = new BoardPosition(move[0], move[1]);
+		const toPosition = new BoardPosition(move[2], move[3]);
+		const fromPositionString = fromPosition.toChessNotationString();
+		const toPositionString = toPosition.toChessNotationString();
+
+		const pieceToMove = this.board.get(fromPositionString);
+		const move = this.board.move({ from: fromPositionString, to: toPositionString });
+
+		//TODO: handle special moves, like castling (information if the move is a special move is contained in the move variable)
+
 		switch (pieceToMove.type) {
 			case PAWN:
 				await this.#movePawn(fromPosition, toPosition);
@@ -58,11 +66,10 @@ export default class BoardController {
 				console.error("The tile from which the piece is to be moved is empty! The piece movement detection algorithm made a mistake!");
 				break;
 		}
-		this.board.move({ from: fromPosition, to: toPosition });
 	}
 
 	async #movePawn(fromPosition, toPosition) {
-		await this.#moveWithoutFurtherLogic(fromPosition, toPosition);
+		await this.#executePhysicalMovesInOrder(fromPosition, toPosition);
 	}
 
 	/**
@@ -73,65 +80,69 @@ export default class BoardController {
 		const helperPiecePositions = this.calculateKnightHelperPiecePositions(fromPosition, toPosition);
 
 		//if the tile near the from-position is empty
-		if (!this.board.get(helperPiecePositions.nearFromPositionFrom)) {
-			return await this.#moveWithStopovers(fromPosition, [helperPiecePositions.nearFromPositionFrom], toPosition);
+		if (!this.board.get(helperPiecePositions.nearFromPositionFrom?.toChessNotationString())) {
+			return await this.#executePhysicalMovesInOrder(fromPosition, helperPiecePositions.nearFromPositionFrom, toPosition);
 		}
 
 		//if the tile near the to-position is empty
-		if (!this.board.get(helperPiecePositions.nearToPositionFrom)) {
-			return await this.#moveWithStopovers(fromPosition, [helperPiecePositions.nearToPositionFrom], toPosition);
+		if (!this.board.get(helperPiecePositions.nearToPositionFrom?.toChessNotationString())) {
+			return await this.#executePhysicalMovesInOrder(fromPosition, helperPiecePositions.nearToPositionFrom, toPosition);
 		}
 
 		//if none of the tiles is empty, a helper piece has to be moved
 		//one position (near from or near to) is always valid
 		let helperFromPositionToUse = helperPiecePositions.nearFromPositionFrom;
 		let helperToPositionToUse = helperPiecePositions.nearFromPositionTo;
-		if (!this.validatePiecePosition(helperToPositionToUse)) {
+		if (!helperToPositionToUse) {
 			helperFromPositionToUse = helperPiecePositions.nearToPositionFrom;
 			helperToPositionToUse = helperPiecePositions.nearToPositionTo;
 		}
 
-		await this.#moveWithoutFurtherLogic(helperFromPositionToUse, helperToPositionToUse);
-		await this.#moveWithStopovers(fromPosition, [helperFromPositionToUse], toPosition);
-		await this.#moveWithoutFurtherLogic(helperToPositionToUse, helperFromPositionToUse);
+		await this.#executePhysicalMovesInOrder(helperFromPositionToUse, helperToPositionToUse);
+		await this.#executePhysicalMovesInOrder(fromPosition, helperFromPositionToUse, toPosition);
+		await this.#executePhysicalMovesInOrder(helperToPositionToUse, helperFromPositionToUse);
 	}
 
 	async #moveBishop(fromPosition, toPosition) {
-		await this.#moveWithoutFurtherLogic(fromPosition, toPosition);
+		await this.#executePhysicalMovesInOrder(fromPosition, toPosition);
 	}
 
 	async #moveRook(fromPosition, toPosition) {
-		await this.#moveWithoutFurtherLogic(fromPosition, toPosition);
+		await this.#executePhysicalMovesInOrder(fromPosition, toPosition);
 	}
 
 	async #moveQueen(fromPosition, toPosition) {
-		await this.#moveWithoutFurtherLogic(fromPosition, toPosition);
+		await this.#executePhysicalMovesInOrder(fromPosition, toPosition);
 	}
 
 	async #moveKing(fromPosition, toPosition) {
-		await this.#moveWithoutFurtherLogic(fromPosition, toPosition);
+		await this.#executePhysicalMovesInOrder(fromPosition, toPosition);
 	}
 
-	async #moveWithoutFurtherLogic(fromPosition, toPosition) {
-		await this.#moveWithStopovers(fromPosition, [], toPosition);
-	}
+	/**
+	 * Executes a sequence of moves one after another for a single piece.
+	 *
+	 * The piece is grabbed at the first position in the array, is dragged
+	 * along the rest of the positions and is released at the last position in the array
+	 * @param {BoardPosition} moves
+	 */
+	async #executePhysicalMovesInOrder(...moves) {
+		if (moves.length < 2) return;
 
-	async #moveWithStopovers(fromPosition, stopOverPositions, toPosition) {
 		try {
 			await fetchArduino(`REQ:RELS:`);
-			await fetchArduino(`REQ:MOVE:${fromPosition}`);
+			await fetchArduino(`REQ:MOVE:${moves[0].toString()}`); //move to the starting position without dragging any piece
 			await fetchArduino(`REQ:GRAB:`);
-			for (const stopOverPosition of stopOverPositions) {
-				await fetchArduino(`REQ:MOVE:${stopOverPosition}`);
+			for (let i = 1; i < moves.length; i++) {
+				await fetchArduino(`REQ:MOVE:${moves[i].toString()}`);
 			}
-			await fetchArduino(`REQ:MOVE:${toPosition}`);
 			await fetchArduino(`REQ:RELS:`);
 		} catch (error) {
 			console.error("Couldn't physically move the piece: " + error);
 		}
 	}
 
-	async waitForPieceMovement() {
+	/* async waitForPieceMovement() {
 		let fromPosition = null;
 		let toPosition = null;
 
@@ -169,47 +180,16 @@ export default class BoardController {
 		const yPositionStr = parseInt(position_int / 8) + 1;
 		const positionInChessNotation = xPositionStr + yPositionStr;
 		return positionInChessNotation;
-	}
+	} */
 
-	getPositionDelta(from, to) {
-		const fromX = from.charCodeAt(0);
-		const fromY = from.charAt(1);
-		const toX = to.charCodeAt(0);
-		const toY = to.charAt(1);
-
-		const deltaX = toX - fromX;
-		const deltaY = toY - fromY;
-
-		return { x: deltaX, y: deltaY };
-	}
-
-	#addPositionRelative(pos, delta) {
-		const posX = pos.charCodeAt(0);
-		const posY = pos.charCodeAt(1);
-		const newPosX = String.fromCharCode(posX + delta.x);
-		const newPosY = String.fromCharCode(posY + delta.y);
-		return newPosX + newPosY;
-	}
-
-	validatePiecePosition(position) {
-		if (!(position.length === 2 || position.length === 3)) return false;
-
-		const x = position.charCodeAt(0);
-		const y = Number(position.charAt(1));
-		if (!y) return false;
-		const isXYValid = x >= "a".charCodeAt(0) && x <= "h".charCodeAt(0) && y >= 1 && y <= 8;
-
-		if (position.length === 2) return isXYValid;
-
-		//else the position is on the edge of two or four tiles
-
-		const edgeAppendix = Number(position.charAt(2));
-		if (!edgeAppendix) return false;
-		return !((y === 1 && edgeAppendix <= 3) || (y === 8 && edgeAppendix >= 7));
-	}
-
+	/**
+	 *
+	 * @param {BoardPosition} knightFromPosition
+	 * @param {BoardPosition} knightToPosition
+	 * @returns and object containing the from and to positions of the two potential helper pieces
+	 */
 	calculateKnightHelperPiecePositions(knightFromPosition, knightToPosition) {
-		const delta = this.getPositionDelta(knightFromPosition, knightToPosition);
+		const delta = knightFromPosition.getDelta(knightToPosition);
 		let nearFromDelta = { x: Math.ceil(delta.x / 2), y: Math.ceil(delta.y / 2) };
 		let nearToDelta = { x: Math.floor(delta.x / 2), y: Math.floor(delta.y / 2) };
 
@@ -219,32 +199,41 @@ export default class BoardController {
 			nearToDelta = tmpDelta;
 		}
 
-		const nearFromPositionFrom = this.#addPositionRelative(knightFromPosition, nearFromDelta);
-		const nearToPositionFrom = this.#addPositionRelative(knightFromPosition, nearToDelta);
+		let nearFromPositionFrom = null;
+		let nearToPositionFrom = null;
+		let nearFromPositionTo = null;
+		let nearToPositionTo = null;
 
-		let nearFromPositionTo = nearFromPositionFrom;
-		let nearToPositionTo = nearToPositionFrom;
+		try {
+			nearFromPositionFrom = knightFromPosition.addDelta(nearFromDelta);
+			nearFromPositionTo = { ...nearFromPositionFrom };
+		} catch (error) {}
+
+		try {
+			nearToPositionFrom = knightFromPosition.addDelta(nearToDelta);
+			nearToPositionTo = { ...nearToPositionFrom };
+		} catch (error) {}
 
 		switch (delta.x + "|" + delta.y) {
 			case "1|-2": //fallthrough
 			case "-2|1":
-				nearFromPositionTo += "1";
-				nearToPositionTo += "9";
+				nearFromPositionTo?.setAppendix(1);
+				nearToPositionTo?.setAppendix(9);
 				break;
 			case "-1|2": //fallthrough
 			case "2|-1":
-				nearFromPositionTo += "9";
-				nearToPositionTo += "1";
+				nearFromPositionTo?.setAppendix(9);
+				nearToPositionTo?.setAppendix(1);
 				break;
 			case "-1|-2": //fallthrough
 			case "2|1":
-				nearFromPositionTo += "3";
-				nearToPositionTo += "7";
+				nearFromPositionTo?.setAppendix(3);
+				nearToPositionTo?.setAppendix(7);
 				break;
 			case "1|2": //fallthrough
 			case "-2|-1":
-				nearFromPositionTo += "7";
-				nearToPositionTo += "3";
+				nearFromPositionTo?.setAppendix(7);
+				nearToPositionTo?.setAppendix(3);
 				break;
 			default:
 				console.error(`No matching delta found (delta: (${delta.x}|${delta.y}))! The algorithm made a mistake!`);
---
+++ RaspberryPi-WebServer/backend/src/api/helpers/BoardPosition.js
@@ -0,0 +1,140 @@
+/**
+ * Represents a tile position on a chessboard
+ * it has the following components:
+ * x - the x-coordinate (a-h) on the chessboard
+ * y - the y-coordinate (1-8) on the chessboard
+ * appendix - this is a number (1-9) used for a finer positioning of the physically moved pieces on the actual real-life board
+ *            It represents half-step-positions like center (5), top left (1), bottom center (2), ... relative to the stored position/x- and y-components.
+ *            The direction represented by the appendix can be read from a standard numpad:
+ *            7 8 9     ↖ ↑ ↗
+ *            4 5 6     ← • →
+ *            1 2 3     ↙ ↓ ↘
+ */
+export default class BoardPosition {
+	/**
+	 *
+	 * @param {string} x
+	 * @param {number} y
+	 * @param {number} appendix default value is 5, which represents the center of a tile
+	 */
+	constructor(x, y, appendix = 5) {
+		if (!BoardPosition.isValid(x, y, appendix)) {
+			throw new Error(`Invalid parameters for class BoardPosition: x=${x}, y=${y}, appendix=${appendix}`);
+		}
+
+		this.x = x;
+		this.y = y;
+		this.appendix = appendix;
+	}
+
+	/**
+	 * Adds a given delta to the current BoardPosition
+	 * @param {object} delta an object containing x and y which represent deltaX and deltaX as a number
+	 * @returns A new BoardPosition with the applied delta
+	 */
+	addDelta(delta) {
+		const newX = String.fromCharCode(this.getX().charCodeAt(0) + delta.x);
+		const newY = this.getY() + delta.y;
+
+		if (!BoardPosition.isValid(newX, newY, this.getAppendix)) {
+			throw new Error("The delta results in a position that lies outside of the board!");
+		}
+
+		return new BoardPosition(newX, newY, this.getAppendix());
+	}
+
+	/**
+	 * The object that the method is called on is the "from" position, the parameter is the "to" position
+	 * @param {BoardPosition} pos2
+	 * @returns an object containing the x and y delta of the two positions
+	 */
+	getDelta(pos2) {
+		const fromXCharCode = this.getX().charCodeAt(0);
+		const fromY = this.getY();
+		const toXCharCode = pos2.getX().charCodeAt(0);
+		const toY = pos2.getY();
+
+		const deltaX = toXCharCode - fromXCharCode;
+		const deltaY = toY - fromY;
+
+		return { x: deltaX, y: deltaY };
+	}
+
+	/**
+	 * Checks if the components for a BoardPosition are valid
+	 * @param {string} x
+	 * @param {number} y
+	 * @param {number} appendix
+	 * @returns {boolean} true if all components are valid, false otherwise
+	 */
+	static isValid(x, y, appendix) {
+		return BoardPosition.#validateX(x) && BoardPosition.#validateY(y) && BoardPosition.#validateAppendix(appendix);
+	}
+
+	/**
+	 * Checks if the x-component for a BoardPosition is valid
+	 * @param {string} x
+	 * @returns {boolean} true if valid, false otherwise
+	 */
+	static #validateX(x) {
+		return x.length == 1 && x.charCodeAt(0) >= "a".charCodeAt(0) && x.charCodeAt(0) <= "h".charCodeAt(0);
+	}
+
+	/**
+	 * Checks if the y-component for a BoardPosition is valid
+	 * @param {number} y
+	 * @returns {boolean} true if valid, false otherwise
+	 */
+	static #validateY(y) {
+		return Number.isInteger(y) && y >= 1 && y <= 8;
+	}
+
+	/**
+	 * Checks if the appendix-component for a BoardPosition is valid
+	 * @param {number} appendix
+	 * @returns {boolean} true if valid, false otherwise
+	 */
+	static #validateAppendix(appendix) {
+		return Number.isInteger(appendix) && appendix >= 1 && appendix <= 9;
+	}
+
+	/**
+	 * @returns {string}
+	 */
+	getX() {
+		return this.x;
+	}
+
+	/**
+	 * @returns {number}
+	 */
+	getY() {
+		return this.y;
+	}
+
+	/**
+	 * @returns {number}
+	 */
+	getAppendix() {
+		return this.appendix;
+	}
+
+	setAppendix(appendix) {
+		if (!BoardPosition.#validateAppendix(appendix)) throw new Error(`The given appendix is not valid: ${appendix}`);
+		this.appendix = appendix;
+	}
+
+	/**
+	 * @returns {string}
+	 */
+	toChessNotationString() {
+		return this.getX() + String(this.getY());
+	}
+
+	/**
+	 * @returns {string}
+	 */
+	toString() {
+		return this.toChessNotationString + String(this.getAppendix());
+	}
+}
